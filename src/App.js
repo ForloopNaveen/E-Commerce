86cc4d2a1c374d079e42a153c6e4c0041478dbee
ForloopNaveen <codemavericknk@gmail.com>
2024-03-03 22:51:45 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Shop } from "./Pages/Shop";
 import { ShopCategory } from "./Pages/ShopCategory";
 import { Product } from "./Pages/Product";
 import { Cart } from "./Pages/Cart";
 import { LoginSignup } from "./Pages/LoginSignup";
 import { Footer } from "./Components/Footer/Footer";
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kids_banner from './Components/Assets/banner_kids.png'
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kids_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
-  return  <div>
+  return (
+    <div>
       <BrowserRouter>
         <Navbar />
-        <h1>Navigate to your product using menu</h1> 
+
         <Routes>
-          <Route path="/" element={<Shop/>}/>
-          <Route path="/mens" element={<ShopCategory  banner={men_banner} category="men"/>}/>
-          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>}/>
-          <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid"/>}/>
-          <Route path="/product" element={<Product/>}>
-            <Route path=":productId" element={<Product/>}/>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/login" element={<LoginSignup/>}/>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
-        <Footer/>
-        </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
     </div>
-  
+  );
 }
 
 export default App;
